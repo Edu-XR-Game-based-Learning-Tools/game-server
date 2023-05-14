ce5fb2eae0cc52aa890cb021d85f5b358954d2c4
Tom <pnthoai165@gmail.com>
2023-05-14 10:57:32 -0700

    Add docker compose for db, update db fields

---
+++ server/GBLT/GBLT.Infrastructure/Migrations/20230416201607_Update-Username-Field.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GBLT.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUsernameField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "User",
+                newName: "Username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_UserName",
+                table: "User",
+                newName: "IX_User_Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "User",
+                newName: "UserName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_Username",
+                table: "User",
+                newName: "IX_User_UserName");
+        }
+    }
+}
